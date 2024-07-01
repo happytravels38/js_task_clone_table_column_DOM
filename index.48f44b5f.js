6a782bfa6522f8da0722e3bbcc96a9890cc034d7
happytravels38 <happytravels38@gmail.com>
2024-07-01 12:30:32 -0400

    make build

---
+++ index.48f44b5f.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelector("table").querySelectorAll("tr").forEach(function(e){var n=e.children,t=n[1].cloneNode(!0);e.insertBefore(t,n[n.length-1])})});
+//# sourceMappingURL=index.48f44b5f.js.map
